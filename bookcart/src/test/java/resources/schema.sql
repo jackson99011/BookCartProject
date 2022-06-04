05ef0c0258b178932d650fdf8e525c784c3b2d6e
Terry <jackson99011@gmail.com>
2022-06-04 15:50:52 +0800

    帳號功能-單元測試

--- bookcart/src/test/java/resources/schema.sql
+++ bookcart/src/test/java/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE product
+CREATE TABLE IF NOT EXISTS product
 (
     product_id         INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
     product_name       VARCHAR(128) NOT NULL,
@@ -7,6 +7,14 @@ CREATE TABLE product
     price              INT          NOT NULL,
     stock              INT          NOT NULL,
     description        VARCHAR(1024),
+    created_date       TIMESTAMP    NOT NULL,
+    last_modified_date TIMESTAMP    NOT NULL
+);
+CREATE TABLE IF NOT EXISTS user
+(
+    user_id            INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email              VARCHAR(256) NOT NULL UNIQUE,
+    password           VARCHAR(256) NOT NULL,
     created_date       TIMESTAMP    NOT NULL,
     last_modified_date TIMESTAMP    NOT NULL
 );
